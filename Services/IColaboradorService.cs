36084586e38a53519da9f383f3b1d4cbef7dbfa5
Elizeu22 <elizeu29.silva@gmail.com>
2025-01-06 09:30:29 -0300

    Adicionado a interface Colaborador

---
+++ Services/IColaboradorService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using CadastroVeiculo.Models;
+
+
+
+
+
+namespace CadastroVeiculo.Services
+{
+    public interface IColaboradorService
+    {
+        Task<IEnumerable<Colaborador>> GetColaboradores();
+        Task<Colaborador> GetColaborador(int id);
+        Task Colaboradores(Colaborador chamados);
+        Task AtualizarColaborador(Colaborador chamados);
+        Task DeletarColaborador(Colaborador chamados);
+    }
+}
